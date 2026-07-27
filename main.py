133b69608b86d077d6799552ba5eef5c0bafb4a0
4-82 <180662930+4-82@users.noreply.github.com>
2026-07-27 22:59:30 +0000

    main: import sys and use book path as cli argument

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import get_book_word_count, get_char_count, chars_dict_to_sorted_list
 
 def get_book_text(filepath: str) -> str:
@@ -15,5 +16,9 @@ def print_report(text: str, count: int, sorted_char_count: list[tuple[str, int]]
     print(f"============ BOOKBOT ============\nAnalyzing book found at {text}...\n----------- Word Count ----------\nFound {count} total words\n--------- Character Count -------\n{string_result}============= END ===============")
 
 def main() -> str:
-    print_report("books/frankenstein.txt", get_book_word_count(get_book_text("books/frankenstein.txt")), chars_dict_to_sorted_list(get_char_count(get_book_text("books/frankenstein.txt"))))
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    print_report(sys.argv[1], get_book_word_count(get_book_text(sys.argv[1])), chars_dict_to_sorted_list(get_char_count(get_book_text(sys.argv[1]))))
 main()
