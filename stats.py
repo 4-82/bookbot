3307bf12deeb74eafbb44ca159b37fcef658c498
4-82 <180662930+4-82@users.noreply.github.com>
2026-07-27 22:26:14 +0000

    stats: add sort_on and chars_dict_to_sorted_list

--- stats.py
+++ stats.py
@@ -7,3 +7,12 @@ def get_char_count(text: str) -> dict[str, int]:
             log[char] += 1
         return log
 
+def sort_on(data: tuple[str, int]) -> int:
+    return data[1]
+
+def chars_dict_to_sorted_list(data: dict[str, int]) -> list[tuple[str, int]]:
+    values = []
+    for key in data:
+        values.append((key, data[key])) 
+    values = sorted(values, key=sort_on, reverse=True)
+    return values
