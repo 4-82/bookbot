8356f616f08f51bb1b5439778ad4b60b13b1688b
4-82 <180662930+4-82@users.noreply.github.com>
2026-07-27 21:44:13 +0000

    main: add get_book_word_count

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+def get_book_word_count(text: str) -> int:
+    return len(text.split())
+
 def get_book_text(filepath: str) -> str:
      with open(filepath) as file:
          file_content = file.read()
