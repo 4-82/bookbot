dc3a3df2cc2f76e91c1b2e5a33840b9947226403
4-82 <180662930+4-82@users.noreply.github.com>
2026-07-27 22:26:40 +0000

    main: import functions and print data

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_book_word_count, get_char_count
+from stats import get_book_word_count, get_char_count, chars_dict_to_sorted_list
 
 def get_book_text(filepath: str) -> str:
      with open(filepath, "r+", encoding="utf-8") as file:
@@ -7,7 +7,8 @@ def get_book_text(filepath: str) -> str:
 
 def main() -> str:
     characters = get_char_count(get_book_text("books/frankenstein.txt"))
-    print(f"Found {get_book_word_count(get_book_text("books/frankenstein.txt"))} total words", characters)
+    data = chars_dict_to_sorted_list(characters)
+    print(f"Found {get_book_word_count(get_book_text("books/frankenstein.txt"))} total words", characters, data)
          
 
 main()
