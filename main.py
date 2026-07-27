395ddaf9c80b082ad23062c8bb18ab3278ae579c
4-82 <180662930+4-82@users.noreply.github.com>
2026-07-27 22:50:41 +0000

    main: add print_report function

--- main.py
+++ main.py
@@ -5,10 +5,13 @@ def get_book_text(filepath: str) -> str:
          file_content = file.read()
      return file_content
 
-def main() -> str:
-    characters = get_char_count(get_book_text("books/frankenstein.txt"))
-    data = chars_dict_to_sorted_list(characters)
-    print(f"Found {get_book_word_count(get_book_text("books/frankenstein.txt"))} total words", characters, data)
-         
+def print_report(text: str, count: int, sorted_char_count: list[tuple[str, int]]) -> str:
+    string_result = ""
+    for data in sorted_char_count:
+        if data[0].isalpha() == False:
+            continue
+        string_result += f"{data[0]}: {data[1]}\n"
+        
+    print(f"============ BOOKBOT ============\nAnalyzing book found at {text}...\n----------- Word Count ----------\nFound {count} total words\n--------- Character Count -------\n{string_result}============= END ===============")
 
 main()
